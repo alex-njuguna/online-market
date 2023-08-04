733631a5348eb199e1020f10f36af1fcdeae846f
Alex Njuguna <123743552+alex-njuguna@users.noreply.github.com>
2023-08-04 20:27:14 +0300

    Update urls.py

--- market/core/urls.py
+++ market/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 from.forms import LoginForm
@@ -12,5 +12,5 @@ urlpatterns = [
     
     path("signup/", views.signup, name="signup"),
     path("login/", LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name="login"),
-    
+    path("logout/", LogoutView.as_view(next_page="core:login"), name="logout"),
 ]
